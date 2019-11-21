e4ca5b7089ee77950a8bd6ad40596ff99616e10d
Phillip Wong <hncswpy@gmail.com>
2019-11-21 21:40:01 +0400

    image resources update

--- network.py
+++ network.py
@@ -1,9 +1,9 @@
 import socket
 
 class Network():
-    def __init__(self):
+    def __init__(self,server):
         self.client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.server = '10.225.66.246'
+        self.server = server
         self.port = 5555
         self.addr = (self.server,self.port)
         self.pos = self.connect()
--- server.py
+++ server.py
@@ -2,43 +2,44 @@ import socket
 from _thread import *
 import sys
 
-server = '10.225.66.246'
 port = 5555
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-try:
-    s.bind((server,port))
-except socket.error as e:
-    str(e)
-
-s.listen(2)
-print('Server loading...')
-
-def thread_client(conn):
-    conn.send(str.encode('Connected'))
-    reply = ''
-    while True:
-        try:
-            data = conn.recv(2048)
-            reply = data.decode('utf-8')
-            string = 'I am Phillip'
-            if not data:
-                print('Disconnected')
-                break
-            else:
-                print('Received: ',reply)
-                print('Sending: ',string)
-
-            conn.sendall(str.encode(string))
+class Server():
+    def __init__(self):
+        self.host_name = socket.gethostname()
+        self.host_ip = socket.gethostbyname(self.hostname)
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
-        except:
-            break
-    
-    print('Connection lost.')
-    conn.close()
+        try:
+            self.server.bind((server,port))
+        except socket.error as e:
+            print(str(e))
+        
+        self.server.listen(1)
+        print('Server loading...')
 
-while True:
-    conn, addr = s.accept()
-    print("Connected to: ",addr)
-    start_new_thread(thread_client,(conn,))
+        conn, addr = self.server.accept()
+        print("Connected to: ",addr)
+        start_new_thread(self.thread_client,(conn))
+
+    def thread_client(conn):
+        conn.send(str.encode('Connected to:'+str(self.host_name)+'@'+str(self.host_ip)))
+        reply = ''
+        while True:
+            try:
+                data = conn.recv(2048).decode('utf-8')
+                
+                if not data:
+                    print('Disconnected')
+                    break
+                else:
+                    print('Received: ',data)
+                    print('Sending: ',string)
+
+                conn.sendall(str.encode(string))
+            
+            except:
+                break
+        
+        print('Connection lost.')
+        conn.close()
